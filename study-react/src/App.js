13f286f54ce7b9047c3b6cb66fee31c994ca8c81
sk Lee <znfnfn2140@gmail.com>
2022-12-02 09:25:28 +0900

    props 추가 주석

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import StateBox from './components/StateBox'; // ??
 import PropsParent from './components/PropsParent';
 import TestState from './components/TestState';
+import EventBox from './components/EventBox';
 
 // App 안에 원하는 내용을 바로 작성할 수 있지만, 
 // 각 내용을 컴포넌트로 만들어서 화면에 출력을 하고 있다.
@@ -19,6 +20,8 @@ function App() {
       <StateBox></StateBox>
       <PropsParent />
       <TestState />
+      
+      <EventBox />
     </div>
   );
 }
---
+++ study-react/src/components/EventBox.jsx
@@ -0,0 +1,9 @@
+const EventBox = () => {
+  return (
+    <div>
+      <h3>이벤트박스</h3>
+    </div>
+  );
+}
+
+export default EventBox;
