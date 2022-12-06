2b9ac1558e1304879f1e1ce8ba2502e1683ac6fa
sk Lee <znfnfn2140@gmail.com>
2022-12-06 12:31:49 +0900

    API와 hook

--- study-react/src/components/APIComp.jsx
+++ study-react/src/components/APIComp.jsx
@@ -1,9 +1,16 @@
+// 데이터 API를 통해서 값(뉴스 데이터)를 가져오는 컴포넌트
+// 데이터 API의 특징
+// - 데이터를 가져오기 위해 Ajax를 사용하여 비동기로 값을 가져와서 사용
+//   (http객체, fetchAPI, axios)
+
 const APIComp = () => {
+  
+  
   return (
     <div>
       
     </div>
   );
 }
- 
+
 export default APIComp;
